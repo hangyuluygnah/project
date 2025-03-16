9ad5090e7bd364dc1fae726575a7757becb275cc
okj <okj001010@gmail.com>
2025-03-16 18:10:26 +0900

    FIXED: achievements_select.js

--- js/achievements_select.js
+++ js/achievements_select.js
@@ -1,8 +1,6 @@
-const items = document.querySelectorAll(".achievements-item");
+const achievementItems = document.querySelectorAll(".achievements-item");
 
-console.log(items);
-
-items.forEach((item) => {
+achievementItems.forEach((item) => {
   item.addEventListener("click", () => {
     const id = item.getAttribute("data-id");
     window.location.href = `achievements_info.html?id=${id}`;
