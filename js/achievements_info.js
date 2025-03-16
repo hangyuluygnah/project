1ca4665f68af42ea2a1149318c67079ac104917b
okj <okj001010@gmail.com>
2025-03-16 18:09:30 +0900

    FIXED: achievements_info.js

--- js/achievements_info.js
+++ js/achievements_info.js
@@ -1,12 +1,15 @@
-const urlParams = new URLSearchParams(window.location.search);
-const currentId = parseInt(urlParams.get("id"), 10);
-const title = document.querySelector(".title");
-
-const title_list = [
+const achievementsData = [
   "Gratitude Journal",
   "Positive Self talk",
   "Anger Acceptance",
   "Planning",
 ];
 
-title.textContent = title_list[currentId - 1];
+// URL 파라미터에서 id 가져오기
+const urlParams = new URLSearchParams(window.location.search);
+const currentId = parseInt(urlParams.get("id"), 10);
+
+const achievementsTitle = document.querySelector(".title");
+
+// ID에 해당하는 title 가져오기
+achievementsTitle.textContent = achievementsData[currentId - 1];
