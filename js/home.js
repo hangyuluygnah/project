ddb6577d6eb971239f7ea7742068eecd36d653f2
hangyuluygnah <hangyulhangyul778@gmail.com>
2025-04-13 19:20:53 +0900

    deletion

--- js/home.js
+++ js/home.js
@@ -9,9 +9,6 @@ const stressInput = document.querySelector("#stressInput");
 const cancelBtn = document.querySelector("#cancelBtn");
 const submitBtn = document.querySelector("#submitBtn");
 
-console.log("home.js loaded!!");
-console.log("I'm so happy to see you here! 😊");
-console.log("Let's start today's habit!");
 
 // "Start today's habit!" 클릭 시 팝업 열기
 startHabitCard.addEventListener("click", () => {
